23ca82ad61aade6db99ab902089a6bec7c2e724b
Md Rahi <fhrfhr102030@gmail.com>
2022-11-08 04:02:17 +0600

    added extra 2 section for home page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAndroid, faTelegram, faSquareWhatsapp} from "@fortawesome/free-brands-svg-icons";
+import img from '../../Assets/Image/contact.jpg';
+
 
 const Contact = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="my-12 grid grid-cols-1 lg:grid-cols-2 gap-2 justify-items-end items-center shadow-lg bg-sky-900 p-4 rounded">
+      <div className="divide-y-4 divide-double divide-slate-400/[.60]">
+        <div className="">
+          <h1 className="text-4xl my-6">Let's Contact Us</h1>
+          <p className="text-lg my-4">
+            Share your excitement with us. We'd love to hear from you. Here's
+            how you can reach us...
+          </p>
         </div>
-    );
+        <div className="">
+          <div className="flex flex-1 gap-3 items-center mt-3">
+            <FontAwesomeIcon
+              className="hover:text-slate-500 text-3xl"
+              icon={faAndroid}
+            ></FontAwesomeIcon>
+            <p>017xxxxxxxx</p>
+          </div>
+          <div className="flex flex-1 gap-3 items-center mt-3">
+            <FontAwesomeIcon
+              className="hover:text-slate-500 text-3xl"
+              icon={faSquareWhatsapp}
+            ></FontAwesomeIcon>
+            <p>name@gmail.com</p>
+          </div>
+          <div className="flex flex-1 gap-3 items-center mt-3">
+            <FontAwesomeIcon
+              className="hover:text-slate-500 text-3xl"
+              icon={faTelegram}
+            ></FontAwesomeIcon>
+            <p>Dhaka, Bangladesh</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img className="rounded w-96 h-72" src={img} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,43 +4,47 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Choose from "../Choose";
+import Contact from "../../Contact/Contact";
 
 const Home = () => {
   return (
-    <div className="card glass my-12 relative">
-      <div className="opacity-50">
-        <img
-          className="object-cover h-96 w-full"
-          src={img}
-          alt="home-bg"
-        />
-      </div>
-      <div className="absolute w-full top-36 left-0 text-center">
-        <h2 className="text-2xl lg:text-3xl mb-8">
-          Travel the world with{" "}
-          <span className="bg-orange-300 p-2 text-xl rounded-lg text-slate-700 font-bold">
-            Rapid Trip
-          </span>
-        </h2>
-        <p className="text-xl mb-12">Work, Travel, Repeat.</p>
-        <div className="">
-          <button className="btn btn-outline btn-success">Explore</button>
+    <div>
+      <div className="card glass my-12 relative">
+        <div className="opacity-50">
+          <img className="object-cover h-96 w-full" src={img} alt="home-bg" />
+        </div>
+        <div className="absolute w-full top-36 left-0 text-center">
+          <h2 className="text-2xl lg:text-3xl mb-8">
+            Travel the world with{" "}
+            <span className="bg-orange-300 p-2 text-xl rounded-lg text-slate-700 font-bold">
+              Rapid Trip
+            </span>
+          </h2>
+          <p className="text-xl mb-12">Work, Travel, Repeat.</p>
+          <div className="">
+            <button className="px-4 py-2 shadow-transparent bg-orange-400 hover:bg-orange-600 text-slate-900 font-bold rounded">
+              Explore
+            </button>
+          </div>
+        </div>
+        <div class="lg:flex md:flex flex-col hidden gap-4 text-3xl absolute bottom-36 right-10 text-slate-100 opacity-70">
+          <FontAwesomeIcon
+            className="hover:text-slate-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+            icon={faFacebook}
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            className="hover:text-slate-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+            icon={faGithub}
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            className="hover:text-slate-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+            icon={faLinkedin}
+          ></FontAwesomeIcon>
         </div>
       </div>
-      <div class="lg:flex md:flex flex-col hidden gap-4 text-3xl absolute bottom-36 right-10 text-slate-100 opacity-70">
-        <FontAwesomeIcon
-          className="hover:text-slate-500"
-          icon={faFacebook}
-        ></FontAwesomeIcon>
-        <FontAwesomeIcon
-          className="hover:text-slate-500"
-          icon={faGithub}
-        ></FontAwesomeIcon>
-        <FontAwesomeIcon
-          className="hover:text-slate-500"
-          icon={faLinkedin}
-        ></FontAwesomeIcon>
-      </div>
+      <Choose></Choose>
+      <Contact></Contact>
     </div>
   );
 };
