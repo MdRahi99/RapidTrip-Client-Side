f9a8bca0012c1d2ba4481568cd67ceecf07909c3
Md Rahi <fhrfhr102030@gmail.com>
2022-11-07 23:14:06 +0600

    initial setup done

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,39 @@
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Services from "../../Pages/Services/Services";
+import Contact from "../../Pages/Contact/Contact";
+import Register from "../../Pages/Login/Register";
+import Login from "../../Pages/Login/Login";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>, 
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: '/services',
+            element: <Services></Services>
+        },
+        {
+            path: '/contact',
+            element: <Contact></Contact>
+        },
+        {
+            path: '/register',
+            element: <Register></Register>
+        },
+        {
+            path: '/login',
+            element: <Login></Login>
+        },
+      ]
+    }
+  ]);
+
+  export default router;
